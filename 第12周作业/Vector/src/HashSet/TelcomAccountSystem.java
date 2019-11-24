29429efcf24efe8e2eaae3fda4cdc7f12f63260e
Yh071027 <1226341796@qq.com>
2019-11-24 22:52:55 +0800

    第12周作业

---
+++ 第12周作业/Vector/src/HashSet/TelcomAccountSystem.java
@@ -0,0 +1,13 @@
+package HashSet;
+
+public class TelcomAccountSystem{
+	public static void main(String[] args){
+		//实例化一个电信用户类TelcomUser
+		TelcomUser telcomUser=new TelcomUser("13782521851");
+		//生成通话记录
+		telcomUser.generateCommubicateRecord();
+		//打印通话详单
+		telcomUser.printDetails();
+		telcomUser.printCallToNumber();
+	}
+}
